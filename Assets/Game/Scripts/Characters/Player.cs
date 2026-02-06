0ae14a1d8ae956fb58f9138c1eeb3ee1ed1db077
Ykrop3301 <ykrop3301@gmail.com>
2026-02-06 13:30:15 +1100

    refactor:  stylistic improvements

--- Assets/Game/Scripts/Characters/Player.cs
+++ Assets/Game/Scripts/Characters/Player.cs
@@ -10,28 +10,21 @@ namespace Characters
 
         void Update()
         {
-            // Получаем ввод от игрока
             float moveVertical = Input.GetAxis("Vertical");
 
-            // Получаем позицию камеры
             Camera cam = Camera.main;
-            // Вычисляем направление вперед и вбок относительно камеры
             Vector3 forward = cam.transform.TransformDirection(Vector3.forward);
             Vector3 right = cam.transform.TransformDirection(Vector3.right);
 
-            // Нормализуем направление
             forward.y = 0;
             right.y = 0;
             forward.Normalize();
             right.Normalize();
 
-            // Рассчитываем движение
             Vector3 movement = forward * moveVertical * speed * Time.deltaTime;
 
-            // Передвижение персонажа
             transform.Translate(movement, Space.World);
 
-            // Вращение игрока в направлении движения
             if (movement != Vector3.zero)
             {
                 Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
